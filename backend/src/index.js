55bf05b2ab049c66684be0bb871887b4ed94cd23
GiovanaNapoli <32879408+GiovanaNapoli@users.noreply.github.com>
2020-03-29 15:46:20 -0300

    Projeto web e mobile
    Projeto web e mobile completos

---
+++ backend/src/index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes.js');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+/**
+ * Rotas / Recurosos
+ */
+
+ /**
+  * Métodos HTTP
+  * 
+  * GET: Buscar/listar uma informação no backend
+  * POST: Criar uma informação no backend
+  * PUT: Alterar uma informação no backend
+  * DELET: Deletar uma informação no backend
+  * 
+  * Tipos de parâmetros
+  * 
+  * Query Params: Parâmetros nomeados e enviados na rota após '?' (Filtros, paginação)
+  * Route Params: Parâmetros utilizados  para identificar recursos
+  * Request body: Corpo da requisição, utilizado para criari ou alterar recursos
+  */
+
+  /**
+   * Bancos de dados
+   * 
+   * SQL: MSQL, SQLite, PostegreSQL, ORACLE, Microsoft SQL server
+   * NoSQL: MongoDB, CounchDB, etc...
+   */
+
+   /**
+    * Driver: SELECT * FROM users
+    * Query Builder: table('users')
+    */
+
+
+app.listen(3333);
---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React , {useState}from 'react';
+import './global.css'
+import Routes from './routes';
+
+// JSX 
+
+function App() {
+  
+  
+  return (
+    <Routes></Routes>
+  );
+}
+
+export default App;
